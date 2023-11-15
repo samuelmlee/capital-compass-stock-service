9a98e57f63266d5d6b9b7c93bc3f9fd8b633d41c
Samuel Lee <sam.leefr@gmail.com>
2023-11-15 10:19:15 +0100

    Initial commit

---
+++ src/main/java/org/capitalcompass/capitalcompassstocks/CapitalCompassStocksApplication.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompassstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCompassStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalCompassStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/capitalcompass/capitalcompassstocks/CapitalCompassStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompassstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalCompassStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
