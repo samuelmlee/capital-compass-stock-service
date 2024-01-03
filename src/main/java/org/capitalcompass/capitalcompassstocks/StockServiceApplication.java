46286c24b8462b75d01b73ae85e7ae771f569401
Samuel Lee <sam.leefr@gmail.com>
2024-01-03 11:35:57 +0100

    renaming

--- src/main/java/org/capitalcompass/capitalcompassstocks/StockServiceApplication.java
+++ src/main/java/org/capitalcompass/capitalcompassstocks/StockServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapitalCompassStocksApplication {
+public class StockServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CapitalCompassStocksApplication.class, args);
+        SpringApplication.run(StockServiceApplication.class, args);
     }
 }
--- src/test/java/org/capitalcompass/capitalcompassstocks/StockServiceApplicationTests.java
+++ src/test/java/org/capitalcompass/capitalcompassstocks/StockServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CapitalCompassStocksApplicationTests {
+class StockServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
