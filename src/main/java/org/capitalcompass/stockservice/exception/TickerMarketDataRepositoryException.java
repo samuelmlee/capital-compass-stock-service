d9d557238aa7d5852c8776904705a60f2f386fd4
Samuel Lee <sam.leefr@gmail.com>
2024-03-13 17:31:41 +0100

    error handling

---
+++ src/main/java/org/capitalcompass/stockservice/exception/TickerMarketDataRepositoryException.java
@@ -0,0 +1,8 @@
+package org.capitalcompass.stockservice.exception;
+
+public class TickerMarketDataRepositoryException extends RuntimeException {
+
+    public TickerMarketDataRepositoryException(String s) {
+        super(s);
+    }
+}
--- src/main/java/org/capitalcompass/stockservice/service/MarketDataUpdateService.java
+++ src/main/java/org/capitalcompass/stockservice/service/MarketDataUpdateService.java
@@ -1,9 +1,13 @@
 package org.capitalcompass.stockservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.capitalcompass.stockservice.api.TickerDetailResult;
 import org.capitalcompass.stockservice.client.ReferenceDataClient;
 import org.capitalcompass.stockservice.entity.TickerMarketData;
+import org.capitalcompass.stockservice.exception.PolygonClientErrorException;
+import org.capitalcompass.stockservice.exception.TickerDetailRepositoryException;
+import org.capitalcompass.stockservice.exception.TickerMarketDataRepositoryException;
 import org.capitalcompass.stockservice.repository.TickerDetailRepository;
 import org.capitalcompass.stockservice.repository.TickerMarketDataRepository;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,6 +18,7 @@ import reactor.core.publisher.Mono;
 import java.time.Instant;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class MarketDataUpdateService {
 
@@ -23,18 +28,31 @@ public class MarketDataUpdateService {
 
     private final TickerMarketDataRepository tickerMarketDataRepository;
 
-
     @Scheduled(cron = "${market-data.fetch-cron}")
     public Disposable saveLatestTickerMarketData() {
         return tickerDetailRepository.findAll()
+                .onErrorResume(e -> {
+                    return Mono.error(new TickerDetailRepositoryException("Error getting all ticker details "));
+                })
                 .flatMap(tickerDetail -> referenceDataClient.getTickerDetails(tickerDetail.getSymbol())
+                        .onErrorResume(e -> {
+                            return Mono.error(new PolygonClientErrorException("Error ticker details for symbol :" + tickerDetail.getSymbol()));
+                        })
                         .flatMap(tickerDetailResponse -> buildTickerMarketDataFromResponse(tickerDetailResponse.getResults(), tickerDetail.getId())))
-                .collectList().flatMapMany(tickerMarketDataRepository::saveAll).subscribe();
+                .collectList().flatMapMany(tickerMarketDataRepository::saveAll)
+                .onErrorResume(e -> {
+                    return Mono.error(new TickerMarketDataRepositoryException("Error saving all ticker market data"));
+                })
+                .subscribe();
     }
 
     @Scheduled(cron = "${market-data.delete-duplicates-cron}")
     public Disposable deleteDuplicateTickerMarketData() {
-        return tickerMarketDataRepository.deleteDuplicateTickerDetail().subscribe();
+        return tickerMarketDataRepository.deleteDuplicateTickerDetail()
+                .onErrorResume(e -> {
+                    return Mono.error(new TickerMarketDataRepositoryException("Error deleting market data duplicates"));
+                })
+                .subscribe();
     }
 
     private Mono<TickerMarketData> buildTickerMarketDataFromResponse(TickerDetailResult result, Long tickerDetailId) {
