3d4916b0c6ba315e3aec4164b654d743ddbe0305
Samuel Lee <sam.leefr@gmail.com>
2024-03-16 00:17:50 +0100

    ArgumentCaptor

--- src/main/java/org/capitalcompass/stockservice/service/MarketDataUpdateService.java
+++ src/main/java/org/capitalcompass/stockservice/service/MarketDataUpdateService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.capitalcompass.stockservice.api.TickerDetailResult;
 import org.capitalcompass.stockservice.client.ReferenceDataClient;
+import org.capitalcompass.stockservice.entity.TickerDetail;
 import org.capitalcompass.stockservice.entity.TickerMarketData;
 import org.capitalcompass.stockservice.exception.PolygonClientErrorException;
 import org.capitalcompass.stockservice.exception.TickerDetailRepositoryException;
@@ -13,9 +14,11 @@ import org.capitalcompass.stockservice.repository.TickerMarketDataRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -28,31 +31,39 @@ public class MarketDataUpdateService {
 
     private final TickerMarketDataRepository tickerMarketDataRepository;
 
+
     @Scheduled(cron = "${market-data.fetch-cron}")
     public Disposable saveLatestTickerMarketData() {
         return tickerDetailRepository.findAll()
-                .onErrorResume(e -> {
-                    return Mono.error(new TickerDetailRepositoryException("Error getting all ticker details "));
-                })
-                .flatMap(tickerDetail -> referenceDataClient.getTickerDetails(tickerDetail.getSymbol())
-                        .onErrorResume(e -> {
-                            return Mono.error(new PolygonClientErrorException("Error ticker details for symbol :" + tickerDetail.getSymbol()));
-                        })
-                        .flatMap(tickerDetailResponse -> buildTickerMarketDataFromResponse(tickerDetailResponse.getResults(), tickerDetail.getId())))
-                .collectList().flatMapMany(tickerMarketDataRepository::saveAll)
-                .onErrorResume(e -> {
-                    return Mono.error(new TickerMarketDataRepositoryException("Error saving all ticker market data"));
-                })
+                .onErrorResume(e -> Mono.error(new TickerDetailRepositoryException("Error getting all ticker details ")))
+                .flatMap(this::fetchAndBuildTickerMarketData)
+                .collectList().flatMapMany(this::saveAllMarketData)
+                .onErrorResume(e -> Mono.error(new TickerMarketDataRepositoryException("Error saving all ticker market data")))
                 .subscribe();
     }
 
     @Scheduled(cron = "${market-data.delete-duplicates-cron}")
     public Disposable deleteDuplicateTickerMarketData() {
         return tickerMarketDataRepository.deleteDuplicateTickerDetail()
-                .onErrorResume(e -> {
-                    return Mono.error(new TickerMarketDataRepositoryException("Error deleting market data duplicates"));
-                })
+                .onErrorResume(e -> Mono.error(new TickerMarketDataRepositoryException("Error deleting market data duplicates")))
                 .subscribe();
+    }
+
+    private Mono<TickerMarketData> fetchAndBuildTickerMarketData(TickerDetail tickerDetail) {
+        return referenceDataClient.getTickerDetails(tickerDetail.getSymbol())
+                .onErrorResume(e -> Mono.error(new PolygonClientErrorException("Error ticker details for symbol :" + tickerDetail.getSymbol())))
+                .flatMap(tickerDetailResponse -> tickerDetailResponse.getResults() == null ? Mono.empty() :
+                        buildTickerMarketDataFromResponse(tickerDetailResponse.getResults(), tickerDetail.getId())
+                );
+    }
+
+    private Flux<TickerMarketData> saveAllMarketData(List<TickerMarketData> tickerMarketDataList) {
+        if (tickerMarketDataList.isEmpty()) {
+            return Flux.empty();
+        } else {
+            return tickerMarketDataRepository.saveAll(tickerMarketDataList);
+
+        }
     }
 
     private Mono<TickerMarketData> buildTickerMarketDataFromResponse(TickerDetailResult result, Long tickerDetailId) {
--- src/test/java/org/capitalcompass/capitalcompassstocks/service/MarketDataUpdateServiceTest.java
+++ src/test/java/org/capitalcompass/capitalcompassstocks/service/MarketDataUpdateServiceTest.java
@@ -4,17 +4,23 @@ import org.capitalcompass.stockservice.api.TickerDetailResponse;
 import org.capitalcompass.stockservice.api.TickerDetailResult;
 import org.capitalcompass.stockservice.client.ReferenceDataClient;
 import org.capitalcompass.stockservice.entity.TickerDetail;
+import org.capitalcompass.stockservice.entity.TickerMarketData;
 import org.capitalcompass.stockservice.repository.TickerDetailRepository;
 import org.capitalcompass.stockservice.repository.TickerMarketDataRepository;
 import org.capitalcompass.stockservice.service.MarketDataUpdateService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -22,50 +28,89 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class MarketDataUpdateServiceTest {
 
+    @Captor
+    ArgumentCaptor<List<TickerMarketData>> marketDataCaptor;
     @Mock
     private ReferenceDataClient referenceDataClient;
-
     @Mock
     private TickerDetailRepository tickerDetailRepository;
-
     @Mock
     private TickerMarketDataRepository tickerMarketDataRepository;
-
     @InjectMocks
     private MarketDataUpdateService marketDataUpdateService;
 
     @Test
     void saveLatestTickerMarketDataOK() {
-        TickerDetail mockDetail = TickerDetail.builder()
+        TickerDetail mockTeslaDetail = TickerDetail.builder()
+                .id(1L)
                 .symbol("TSLA")
-                .name("Tesla, Inc.")
-                .market("stocks")
-                .primaryExchange("NASDAQ")
-                .type("Equity")
                 .build();
 
-        TickerDetailResult mockResult = TickerDetailResult.builder()
+        TickerDetail mockAppleDetail = TickerDetail.builder()
+                .id(2L)
+                .symbol("AAPL")
+                .build();
+
+
+        TickerDetailResult mockTeslaResult = TickerDetailResult.builder()
                 .symbol("TSLA")
-                .name("Tesla, Inc.")
-                .market("stocks")
-                .primaryExchange("NASDAQ")
-                .type("Equity")
+                .marketCap(10L)
+                .shareClassSharesOutstanding(10L)
+                .weightedSharesOutstanding(10L)
                 .build();
 
-        TickerDetailResponse mockResponse = TickerDetailResponse.builder()
-                .results(mockResult)
+        TickerDetailResponse mockTeslaResponse = TickerDetailResponse.builder()
+                .results(mockTeslaResult)
                 .build();
 
-        when(tickerDetailRepository.findAll()).thenReturn(Flux.just(mockDetail));
-        when(referenceDataClient.getTickerDetails(anyString())).thenReturn(Mono.just(mockResponse));
+        TickerDetailResult mockAppleResult = TickerDetailResult.builder()
+                .symbol("AAPL")
+                .marketCap(20L)
+                .shareClassSharesOutstanding(20L)
+                .weightedSharesOutstanding(20L)
+                .build();
+
+        TickerDetailResponse mockAppleResponse = TickerDetailResponse.builder()
+                .results(mockAppleResult)
+                .build();
+
+
+        when(tickerDetailRepository.findAll()).thenReturn(Flux.just(mockTeslaDetail, mockAppleDetail));
+        when(referenceDataClient.getTickerDetails(anyString()))
+                .thenReturn(Mono.just(mockTeslaResponse))
+                .thenReturn(Mono.just(mockAppleResponse));
         when(tickerMarketDataRepository.saveAll(any(Iterable.class))).thenReturn(Flux.empty());
 
 
         marketDataUpdateService.saveLatestTickerMarketData();
 
         verify(tickerDetailRepository, times(1)).findAll();
-        verify(referenceDataClient, times(1)).getTickerDetails(anyString());
-        verify(tickerMarketDataRepository, times(1)).saveAll(any(Iterable.class));
+        verify(referenceDataClient, times(2)).getTickerDetails(anyString());
+        verify(tickerMarketDataRepository, times(1)).saveAll(marketDataCaptor.capture());
+
+        List<TickerMarketData> capturedArguments = marketDataCaptor.getValue();
+
+        TickerMarketData teslaMarketData = capturedArguments.get(0);
+        assertEquals(teslaMarketData.getTickerDetailId(), 1L);
+        assertEquals(teslaMarketData.getMarketCap(), 10L);
+
+        TickerMarketData appleMarketData = capturedArguments.get(1);
+        assertEquals(appleMarketData.getTickerDetailId(), 2L);
+        assertEquals(appleMarketData.getMarketCap(), 20L);
     }
+
+    @Test
+    void saveLatestTickerMarketDataEmptyListOK() {
+
+        when(tickerDetailRepository.findAll()).thenReturn(Flux.empty());
+        marketDataUpdateService.saveLatestTickerMarketData();
+
+        verify(tickerDetailRepository, times(1)).findAll();
+
+        verify(referenceDataClient, never()).getTickerDetails(any());
+        verify(tickerMarketDataRepository, never()).saveAll(any(Iterable.class));
+
+    }
+
 
 }
