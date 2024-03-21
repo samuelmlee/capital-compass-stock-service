319658a861202f361759d1e46b1decc74a9063ed
Samuel Lee <sam.leefr@gmail.com>
2024-03-21 23:02:30 +0100

    saveLatestTickerMarketData save all error

--- src/test/java/org/capitalcompass/capitalcompassstocks/service/MarketDataUpdateServiceTest.java
+++ src/test/java/org/capitalcompass/capitalcompassstocks/service/MarketDataUpdateServiceTest.java
@@ -18,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -186,6 +187,58 @@ public class MarketDataUpdateServiceTest {
         verify(tickerMarketDataRepository, times(1)).saveAll(any(Iterable.class));
 
         assertThat(output.getOut()).contains("Error getting ticker details for symbol");
+    }
+
+    @Test
+    void saveLatestTickerMarketSaveAllError(CapturedOutput output) {
+        TickerDetail mockTeslaDetail = TickerDetail.builder()
+                .id(1L)
+                .symbol("TSLA")
+                .build();
+
+        TickerDetail mockAppleDetail = TickerDetail.builder()
+                .id(2L)
+                .symbol("AAPL")
+                .build();
+
+
+        TickerDetailResult mockTeslaResult = TickerDetailResult.builder()
+                .symbol("TSLA")
+                .marketCap(10L)
+                .shareClassSharesOutstanding(10L)
+                .weightedSharesOutstanding(10L)
+                .build();
+
+        TickerDetailResponse mockTeslaResponse = TickerDetailResponse.builder()
+                .results(mockTeslaResult)
+                .build();
+
+        TickerDetailResult mockAppleResult = TickerDetailResult.builder()
+                .symbol("AAPL")
+                .marketCap(20L)
+                .shareClassSharesOutstanding(20L)
+                .weightedSharesOutstanding(20L)
+                .build();
+
+        TickerDetailResponse mockAppleResponse = TickerDetailResponse.builder()
+                .results(mockAppleResult)
+                .build();
+
+
+        when(tickerDetailRepository.findAll()).thenReturn(Flux.just(mockTeslaDetail, mockAppleDetail));
+        when(referenceDataClient.getTickerDetails(anyString()))
+                .thenReturn(Mono.just(mockTeslaResponse))
+                .thenReturn(Mono.just(mockAppleResponse));
+        when(tickerMarketDataRepository.saveAll(any(Iterable.class)))
+                .thenReturn(Flux.error(new DataIntegrityViolationException("No id in ticker_id matches  ticker_detail_id")));
+
+        marketDataUpdateService.saveLatestTickerMarketData();
+
+        verify(tickerDetailRepository, times(1)).findAll();
+        verify(referenceDataClient, times(2)).getTickerDetails(anyString());
+        verify(tickerMarketDataRepository, times(1)).saveAll(anyIterable());
+
+        assertThat(output.getOut()).contains("Error saving all Ticker Market Data");
     }
 
 
